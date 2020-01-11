cc5405ce817ba005a8a46bd467349b8e92c02142
Shota HAMADA <maku693@outlook.com>
2020-01-12 03:06:24 +0900

    Add command line options

--- imgdim.go
+++ imgdim.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image"
 	_ "image/gif"
@@ -9,12 +10,34 @@ import (
 	"os"
 )
 
+var in string
+
+func init() {
+	flag.StringVar(&in, "in", "", "input file")
+}
+
 func main() {
-	config, format, err := image.DecodeConfig(os.Stdin)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
+	flag.Parse()
+
+	reader := os.Stdin
+	if in != "" {
+		file, err := os.Open(in)
+		if err != nil {
+			fatal(err.Error())
+		}
+		reader = file
 	}
+
+	config, format, err := image.DecodeConfig(reader)
+	if err != nil {
+		fatal(err.Error())
+	}
+
 	fmt.Println("format\twidth\theight")
 	fmt.Println(fmt.Sprintf("%s\t%d\t%d", format, config.Width, config.Height))
+}
+
+func fatal(a ...interface{}) {
+	fmt.Fprintln(os.Stderr)
+	os.Exit(1)
 }
